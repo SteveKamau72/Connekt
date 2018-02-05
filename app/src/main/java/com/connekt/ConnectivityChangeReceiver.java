154b3ae6c444ffc6a5f22219ed3167ddc3262d4e
SteveKamau <stevekamau72@gmail.com>
2018-02-05 08:54:13 +0300

    First commit

---
+++ app/src/main/java/com/connekt/ConnectivityChangeReceiver.java
@@ -0,0 +1,160 @@
+package com.connekt;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * <p>
+ * A broadcast receiver (receiver) is an Android component which allows you to register for system
+ * or application events. All registered receivers for an event are notified by the Android runtime
+ * once this event happens.
+ */
+
+public class ConnectivityChangeReceiver extends BroadcastReceiver {
+    String connectionType;
+    private static boolean firstConnect = true;
+    SharedPreferences sharedPreferences;
+
+    /**
+     * This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+     **/
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        //initialize SharedPreferences for persistence of data
+        sharedPreferences = context.getSharedPreferences("ACCOUNT", context.MODE_PRIVATE);
+
+        //check for network connectivity
+        final ConnectivityManager connMgr = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        final android.net.NetworkInfo wifi = connMgr
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        final android.net.NetworkInfo mobile = connMgr
+                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if (wifi.isConnected()) {
+            connectionType = "Wifi";
+            if (firstConnect) {
+                firstConnect = false;
+                networkRequest(context);
+                updateViewOnConnectivityStatus(true, context);
+                Log.e("network_______", "connected");
+            }
+        } else if (mobile.isConnected()) {
+            connectionType = "Mobile data";
+            if (firstConnect) {
+                // do subroutines here
+                firstConnect = false;
+                networkRequest(context);
+                updateViewOnConnectivityStatus(true, context);
+                Log.e("network_______", "connected");
+            }
+        } else {
+            firstConnect = true;
+            Log.e("network_______", "disconnected");
+            saveOfflineDateToPreferences();
+            updateViewOnConnectivityStatus(false, context);
+        }
+
+    }
+
+    /**
+     * Update UI when network changes
+     **/
+    private void updateViewOnConnectivityStatus(final boolean isConnected, Context context) {
+        if (MainActivity.getInstance() != null) {//only if view is visible
+            MainActivity.getInstance().updateView(isConnected);
+        }
+
+    }
+
+    /**
+     * Making the request using loopj AsyncHttpClient.
+     * Requires @params: imei_code, active_time, last_active_time, type
+     **/
+    private void networkRequest(Context context) {
+        final String url = context.getResources().getString(R.string.base_url) + "connected.php";
+        final int DEFAULT_TIMEOUT = 20 * 10000;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(DEFAULT_TIMEOUT);
+        final RequestParams params = new RequestParams();
+        params.put("imei_code", getDeviceIMEI(context));
+        params.put("active_time", getCurrentTime());
+        params.put("last_active_time", getLastActiveTime());
+        params.put("type", connectionType);
+
+        client.get(url, params, new AsyncHttpResponseHandler() {
+            @Override
+
+            public void onStart() {
+                Log.e("sync__", params.toString());
+            }
+
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                String s = new String(responseBody);
+                Log.e("sync__", s);
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+                Log.e("sync__", error.getMessage());
+            }
+        });
+    }
+
+    /**
+     * Using SharedPreferences to save last active time
+     **/
+    private void saveOfflineDateToPreferences() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("last_active_time", getCurrentTime());
+        editor.apply();
+    }
+
+    /**
+     * Fetch last active time from SharedPreferences
+     **/
+    private String getLastActiveTime() {
+        return sharedPreferences.getString("last_active_time", getCurrentTime());
+    }
+
+
+    /**
+     * Method to return device IMEI. This requires explicit permissions that need to be granted from
+     * Android 6.0 and greater.
+     **/
+    @SuppressLint({"MissingPermission", "HardwareIds"})
+    public String getDeviceIMEI(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (new MainActivity().isPermissionGranted()) {
+            return telephonyManager.getDeviceId();
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Method to return the current date time using SimpleDateFormat
+     **/
+    public String getCurrentTime() {
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());//dd/MM/yyyy
+        Date now = new Date();
+        return sdfDate.format(now);
+    }
+}
---
+++ app/src/main/java/com/connekt/MainActivity.java
@@ -0,0 +1,131 @@
+package com.connekt;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvStatus;
+    ImageView imgStatus;
+    RelativeLayout rootLayout;
+    private static MainActivity mainActivityRunningInstance;
+
+    public static MainActivity getInstance() {
+        return mainActivityRunningInstance;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Get activity's running instance
+        mainActivityRunningInstance = this;
+
+        // Hiding Title bar of this activity screen
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        //Making this activity, full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        // At activity startup we manually check the internet status and change
+        // the connectivity status
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            updateView(true);
+        } else {
+            updateView(false);
+        }
+
+        //Request user for required permissions
+        isPermissionGranted();
+    }
+
+    /**
+     * Initialize views
+     **/
+    private void initViews() {
+        tvStatus = findViewById(R.id.status);
+        imgStatus = findViewById(R.id.img_status);
+        rootLayout = findViewById(R.id.root_layout);
+    }
+
+    /**
+     * Allows read only access to phone state, including the phone number of the device, IMEI, current
+     * cellular network information, the status of any ongoing calls, and a list of any PhoneAccounts
+     * registered on the device.
+     * Protection level: dangerous
+     **/
+    public boolean isPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.READ_PHONE_STATE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v("TAG", "Permission is granted");
+                return true;
+            } else {
+                Log.v("TAG", "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("TAG", "Permission is granted");
+            return true;
+        }
+    }
+
+    /**
+     * Permission results callback
+     **/
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+                    //do ur specific task after read phone state granted
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    /**
+     * Update UI when network changes
+     **/
+    public void updateView(boolean isConnected) {
+        if (isConnected) {
+            tvStatus.setText("Internet Connected");
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.connectedColor));
+            imgStatus.setImageDrawable(getResources().getDrawable(R.drawable.ic_connected));
+        } else {
+            tvStatus.setText("Internet Disconnected");
+            rootLayout.setBackgroundColor(getResources().getColor(R.color.disconnectedColor));
+            imgStatus.setImageDrawable(getResources().getDrawable(R.drawable.ic_disconnected));
+        }
+    }
+}
