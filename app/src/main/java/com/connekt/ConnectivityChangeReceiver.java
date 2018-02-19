e511d7f9b832d831c342c6430a53989f415d68c4
SteveKamau <stevekamau72@gmail.com>
2018-02-19 10:25:00 +0300

    Changed request to POST

--- app/src/main/java/com/connekt/ConnectivityChangeReceiver.java
+++ app/src/main/java/com/connekt/ConnectivityChangeReceiver.java
@@ -105,12 +105,12 @@ public class ConnectivityChangeReceiver extends BroadcastReceiver {
     }
 
     /**
-     * Making the request using loopj AsyncHttpClient.
+     * Making the request using volley.
      * Requires @params: imei_code, active_time, last_active_time, type
      **/
     private void networkRequest(final Context context) {
         final String url = context.getResources().getString(R.string.base_url) + "connected.php";
-        StringRequest strReq = new StringRequest(Request.Method.GET,
+        StringRequest strReq = new StringRequest(Request.Method.POST,
                 url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
